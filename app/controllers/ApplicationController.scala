a3a4114ff0bde5e40bc3463d514cb0bbeb614e52
V-Yaroslavskyi <vovasy96@gmail.com>
2017-02-11 16:01:09 +0200

    a

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -36,7 +36,7 @@ class ApplicationController @Inject()(ws: WSClient, conf: play.api.Configuration
 
     val bodyParts = List(
       new StringPart("url", "https://52.174.38.160/", "UTF-8"),
-      new FilePart("certificate", new File("public/certificates/nginx.crt"))
+      new FilePart("certificate", new File("/home/vova/TradeHubBot/public/certificates/nginx.crt"))
     )
     val client = ws.underlying.asInstanceOf[AsyncHttpClient]
 
