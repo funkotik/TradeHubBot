65f8a24f489b1ffff4565f478f0aeabd664bd449
V-Yaroslavskyi <vovasy96@gmail.com>
2017-02-11 15:59:46 +0200

    webhook

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -3,10 +3,13 @@ package controllers
 import java.io.File
 import javax.inject.{Inject, Singleton}
 
-import com.pengrad.telegrambot.TelegramBotAdapter
-import com.pengrad.telegrambot.request.SetWebhook
+import com.ning.http.client.{AsyncCompletionHandler, AsyncHttpClient, Response}
+import com.ning.http.client.multipart.StringPart
 import play.api.libs.json.Json
 import play.api.libs.ws.{WSClient, WSResponse}
+import com.ning.http.client.multipart.FilePart
+import scala.concurrent.Promise
+import play.api.libs.ws.ning.NingWSResponse
 import play.api.mvc.{Action, Controller, MultipartFormData}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -18,7 +21,7 @@ class ApplicationController @Inject()(ws: WSClient, conf: play.api.Configuration
 
   val url = s"https://api.telegram.org/bot${conf.getString("token").get}"
 
-  val webhookStatus = setWebhook
+  val webhookStatus = setWebhook.map(println)
 
   def index = Action {
     Ok("lol")
@@ -30,10 +33,31 @@ class ApplicationController @Inject()(ws: WSClient, conf: play.api.Configuration
   }
 
   def setWebhook = {
-    val bot = TelegramBotAdapter.build(conf.getString("token").get)
-    val request = new SetWebhook()
-      .url("url")
-      .certificate(new File("public/certificates/nginx.crt"))
-    bot.execute(request)
+
+    val bodyParts = List(
+      new StringPart("url", "https://52.174.38.160/", "UTF-8"),
+      new FilePart("certificate", new File("public/certificates/nginx.crt"))
+    )
+    val client = ws.underlying.asInstanceOf[AsyncHttpClient]
+
+    val builder = client.preparePost(url)
+
+    builder.setHeader("Content-Type", "multipart/form-data")
+    bodyParts.foreach(builder.addBodyPart)
+
+    var result = Promise[NingWSResponse]()
+
+    client.executeRequest(builder.build(), new AsyncCompletionHandler[Response]() {
+      override def onCompleted(response: Response) = {
+        result.success(NingWSResponse(response))
+        response
+      }
+
+      override def onThrowable(t: Throwable) = {
+        result.failure(t)
+      }
+    })
+
+    result.future
   }
 }
