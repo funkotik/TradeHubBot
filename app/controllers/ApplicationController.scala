a20dfa880b8d932b3fe0bdd219b639a4816cb11d
V-Yaroslavskyi <vovasy96@gmail.com>
2017-02-12 14:47:07 +0200

    commands

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -6,7 +6,7 @@ import javax.inject.{Inject, Singleton}
 import telegram._
 import com.ning.http.client.{AsyncCompletionHandler, AsyncHttpClient, Response}
 import com.ning.http.client.multipart.StringPart
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSClient, WSResponse}
 import com.ning.http.client.multipart.FilePart
 import org.json4s
@@ -43,36 +43,55 @@ class ApplicationController @Inject()(ws: WSClient, conf: play.api.Configuration
 
   def toJson[T](t: T): String = compact(render4s(Extraction.decompose(t).underscoreKeys))
 
-  def toAnswer[T](t: T, method: String): String =
-    compact(
+  def toAnswerJson[T](t: T, method: String): JsValue =
+    Json.parse(
+      compact(
       render4s(new JObject(Extraction.decompose(t).asInstanceOf[JObject].obj ++
         List("method" -> JString(method))).underscoreKeys)
+    )
     )
 
   def fromJson[T: Manifest](json: String): T = parse4s(json).camelizeKeys.extract[T]
 
 
   def index = Action {request =>
-    println(request)
     Ok("lol")
   }
 
   def inbox = Action { request =>
     val js = request.body.asJson.get
     val update = fromJson[Update](js.toString)
-    val response = update.message map {x =>
-      SendMessage(Left(x.chat.id), "Пока что я слишком слаб чтобы понять это")
+    val response = update.message map {msg =>
+      val command = getCommand(msg)
+      println(command)
+      command match {
+
+        case Some("/start") => start(msg)
+        case _ => SendMessage(Left(msg.chat.id), "Пока что я слишком слаб чтобы понять это")
+      }
+
     }
-    println(toAnswer(response.get, "sendMessage"))
-    Ok(Json.parse(toAnswer(response.get, "sendMessage")))
+
+
+
+    response.map(x => Ok(toAnswerJson(x, x.methodName))).getOrElse(Ok())
   }
 
-  def sendMessage = {
-
+  def start(msg: Message) = {
+    SendMessage(Left(msg.chat.id),
+      "Я брокер-бот, мониторю торговые площадки, делаю прогнозы рынков," +
+      " помогаю заключить контракт, организовываю торговые коммуникации")
   }
 
+  def getCommand(msg: Message): Option[String] = {
+    msg.entities.getOrElse(Seq()).find { me =>
+      me.`type` == "bot_command" && me.offset == 0
+    }.flatMap{me =>
+      msg.text.map(_.slice(0, me.length))
+    }
+  }
 
-  def setWebhook = {
+  def setWebhook: Future[NingWSResponse] = {
 
     val bodyParts = List(
       new StringPart("url", "https://52.174.38.160/webhook", "UTF-8"),
